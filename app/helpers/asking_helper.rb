5e7d853a6e05ac96c5521107c50659d201921884
MarieCichon <marie-chn@hotmail.fr>
2021-06-07 17:38:13 +0200

    done

---
+++ app/helpers/asking_helper.rb
@@ -0,0 +1,2 @@
+module AskingHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'asking#ask'
+  get '/answer', to: 'asking#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
